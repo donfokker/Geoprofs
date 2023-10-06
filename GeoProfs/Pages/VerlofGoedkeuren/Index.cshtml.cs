3c238ac564713f708e006d320fcf3d7c4b23c68a
DoniBoye <donfokker@gmail.com>
2023-10-06 13:07:01 +0200

    update

---
+++ GeoProfs/Pages/VerlofGoedkeuren/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeoProfs.Pages.VerlofGoedkeuren
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
